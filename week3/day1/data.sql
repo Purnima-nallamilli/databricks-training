d7e84a69e6c8635fcfa5d8d1cbbc8d19c55e18d7
Purnima-nallamilli <nallamillipurnima@gmail.com>
2026-05-26 20:39:11 +0530

    Create data.sql

---
+++ week3/day1/data.sql
@@ -0,0 +1,19 @@
+CREATE TABLE employees (
+    emp_id INT,
+    emp_name VARCHAR(50),
+    department VARCHAR(50),
+    salary INT,
+    join_date DATE
+);
+
+INSERT INTO employees VALUES
+(1,'Amit','Chennai',2000,'2023-01-01'),
+(2,'Ravi','Hyderabad',1500,'2023-01-02'),
+(3,'Sneha','Chennai',3000,'2023-01-03'),
+(4,'Kiran','Bangalore',2500,'2023-01-04'),
+(5,'Priya','Chennai',2000,'2023-01-05'),
+(6,'Arjun','Hyderabad',1800,'2023-01-06'),
+(7,'Neha','Bangalore',2200,'2023-01-07'),
+(8,'Vikas','Chennai',3000,'2023-01-08'),
+(9,'Anjali','Hyderabad',1700,'2023-01-09'),
+(10,'Rahul','Bangalore',2600,'2023-01-10');
